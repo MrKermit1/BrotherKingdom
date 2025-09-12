0daaf39e1176265825c1652a5c094a5b82335583
MrKermit1 <olafisz.pl@gmail.com>
2025-09-12 23:14:07 +0200

    added globals

--- src/globals.cpp
+++ src/globals.cpp
@@ -1,22 +1,48 @@
 #include "globals.h"
 #include "raylib.h"
-Player player = Player();
-World world = World(&player);
 
-int SizeOfChar(const char* text)
+Player player = Player();
+World world = World();
+UI ui;
+
+int SCREEN_WIDTH = 1900;
+int SCREEN_HEIGHT = 1000;
+
+std::vector<Component> uiComponents;
+std::vector<Button> buttons;
+
+size_t SizeOfChar(const char* text)
 {
 	std::string textInString(text);
 	return textInString.size();
 }
 
-bool IsMouseOnEntity(Vector2 entityPosition, Vector2 entitySize)
+bool IsMouseOnEntity(Vector2 entityPosition, Vector2 entitySize, Vector2 mousePosition)
 {
-    Vector2 mousePosition = GetScreenToWorld2D(GetMousePosition(), *(world.GetCamera()));
-    return (
+    return 
+    (
         IsMouseButtonPressed(MOUSE_BUTTON_LEFT)
         && mousePosition.x >= entityPosition.x
         && !(mousePosition.x > entityPosition.x + entitySize.x)
         && mousePosition.y >= entityPosition.y
         && !(mousePosition.y > entityPosition.y + entitySize.y)
-   );
+    );
 }
+
+int RoundUp(int n, int multiplier)
+{
+    if (multiplier == 0)
+    {
+        return n;
+    }
+
+    int divisionRest = n % multiplier;
+
+    if (divisionRest == 0)
+    {
+        return n;
+    }
+
+    return n + multiplier - divisionRest;
+}
+
--- src/globals.h
+++ src/globals.h
@@ -1,9 +1,21 @@
 #pragma once
 #include "world/world.h"
-#include "player/player.h"
+#include "client/player/player.h"
+#include "client/ui/ui.h"
+#include "client/ui/components/component/component.h"
+#include "client/ui/components/button/button.h"
 #include <iostream>
+
+extern int SCREEN_WIDTH;
+extern int SCREEN_HEIGHT;
+
 extern Player player;
 extern World world;
+extern UI ui;
 
-int SizeOfChar(const char* text);
-bool IsMouseOnEntity(Vector2 entityPosition, Vector2 entitySize);
+extern std::vector<Component> uiComponents;
+extern std::vector<Button> buttons;
+
+size_t SizeOfChar(const char* text);
+bool IsMouseOnEntity(Vector2 entityPosition, Vector2 entitySize, Vector2 mousePosition);
+int RoundUp(int n, int multiplier);
