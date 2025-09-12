cf10175c6110b153d1e206a2f4c050afe1485a49
MrKermit1 <olafisz.pl@gmail.com>
2025-09-12 23:23:34 +0200

    deleted comment separators, setting walkable

---
+++ src/world/terrain/terrainnode.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "raylib.h"
+class TerrainNode
+{
+public:
+	enum TerrainType
+	{
+		GRASS,
+		FOREST,
+		STONE,
+		OCCUPIED,
+	};
+
+	static const float NODE_SIZE;
+
+	TerrainNode();
+	TerrainNode(Vector2 pos, TerrainNode::TerrainType type);
+
+	bool IsClicked() const;
+	bool IsWalkable() const;
+	bool operator==(const TerrainNode& other) const;
+
+	Vector2 OnClick(Camera2D* camera);
+	TerrainType GetType();
+	Vector2 GetPosition();
+
+	void SetType(TerrainType type);
+	void SetPosition(Vector2 pos);
+	void UnClick();
+	void Draw();
+private:
+	bool clicked;
+	bool walkable;
+
+	Vector2 position;
+	Texture2D texture;
+	TerrainType type;
+
+	void SwitchTexture();
+};
+
